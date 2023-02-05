2dafdfe92b43541caaa1a55b64ee280c93549d14
Tyzerman19 <97551923+Tyzerman19@users.noreply.github.com>
2023-02-05 12:39:19 -0500

    Create daily_email.py

---
+++ daily_email.py
@@ -0,0 +1,22 @@
+# write a script to take my weather data and send an email to myself every morning with the forecast
+
+# import libraries
+import pandas as pd
+
+
+# grab the weather data
+
+data = pd.read_csv(r'C:\Users\TPC19\Dropbox\Coding\Python Folder\weather_data.csv')
+print(data)
+
+
+# create a dataframe for the day's weather
+
+
+
+# create graphs/visualizations
+## line graph for upcoming day including temperature, precipitation
+## line graph for upcoming day 
+## table of hourly data including tempterature
+
+# package as email and send to me
