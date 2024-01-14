6884874348dc39eda9486d701bc4e07ebe9cd2a4
Tyzerman19 <97551923+Tyzerman19@users.noreply.github.com>
2024-01-14 16:27:00 -0500

    Update shootsim.py

--- shootsim.py
+++ shootsim.py
@@ -2,6 +2,7 @@
 
 from time import sleep
 import random
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,6 +10,25 @@ import matplotlib.pyplot as plt
 # a simulation of an arbitrary 'shooting' game
 # higher 'skill' rating should lead to better scores in the shooting game
 # assume skill level range will be from 1 to 5
+
+# dict of players - ("Name" : "Skill")
+player_dict = {
+    "Mike":5,
+    "Jill":4,
+    "Puppy":3,
+    "Brent":2,
+    "Egg":1
+}
+
+player_list = list(player_dict.keys())
+
+# pick 2 players for the game
+player1 = random.choice(player_list)
+player2 = random.choice(player_list)
+
+player1_skill = player_dict.get(player1)
+player2_skill = player_dict.get(player2)
+
 
 def shoot(skill):
     # function to calculate the score of the shot attempt
@@ -59,7 +79,7 @@ def play_round(p1skill, p2skill, shots_per_round):
     # return the result of the round
     return round_result
 
-def play_match(number_of_rounds, games_per_round, p1skill, p2skill):
+def play_match(game_id, number_of_rounds, games_per_round, p1name, p1skill, p2name, p2skill):
     # function to complete a match by playing multiple rounds
     # intakes the number of rounds/games per round and the skill of each player
 
@@ -67,9 +87,6 @@ def play_match(number_of_rounds, games_per_round, p1skill, p2skill):
     match_result = [0,0,0]
     # store the result of each individual match
     match_list = []
-
-    # set the default outcome to be a tie
-    outcome = "Tie"
 
     # for the number of rounds requested play a round
     for i in range(number_of_rounds):
@@ -90,14 +107,53 @@ def play_match(number_of_rounds, games_per_round, p1skill, p2skill):
 
     # determine the winner
     if match_result[0] > match_result[1]:
-        outcome = "Player 1 wins"
-    if match_result[1] > match_result[0]:
-        outcome = "Player 2 wins"
+        result = "Winner"
+        winner = p1name
+        winner_skill = p1skill
+        loser = p2name
+        loser_skill = p2skill
+    elif match_result[1] > match_result[0]:
+        result = "Winner"
+        winner = p2name
+        winner_skill = p2skill
+        loser = p1name
+        loser_skill = p1skill
+    else:
+        result = "Tie"
+        winner = "None"
+        winner_skill = "N/A"
+        loser = "None"
+        loser_skill = "N/A"
+
+    player1_score = match_result[0]
+    player2_score = match_result[1]
+
+    # package the outcome to return
+    outcome = [game_id,
+                result,
+                [winner, winner_skill,
+                    max(player1_score, player2_score)],
+                [loser, loser_skill,
+                    min(player1_score, player2_score)],
+                match_result]
 
     # print out the results of the test match
     print(outcome)
-    print(match_result)
-    print(match_list)
+
 
 # run a test match
-play_match(25,7,5,5)
+for i in range(18):
+    game_id = i
+    # pick 2 players for the game
+    player1 = random.choice(player_list)
+    player2 = random.choice(player_list)
+    player1_skill = player_dict.get(player1)
+    player2_skill = player_dict.get(player2)
+
+    play_match(game_id,
+               5,
+               7,
+               player1,
+               player1_skill,
+               player2,
+               player2_skill)
