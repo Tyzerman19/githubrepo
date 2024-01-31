e10fd70a9f78a15e1c0b4b3108eec240100725e7
Tyzerman19 <97551923+Tyzerman19@users.noreply.github.com>
2024-01-31 10:36:53 -0500

    edits and comments

--- JRE Words.py
+++ JRE Words.py
@@ -1,17 +1,18 @@
+# import libraries
 import csv
-import time
-
 import requests
-import pandas as pd
 
-# 9318 id numbers with some missing
-
+# write a script to extract transcript information from the JRE podcast
+# https://ogjre.com/transcripts has links to pages containing transcripts
+# transcripts are only available for more recent episodes but also shorter clips
+# 9318 id numbers for videos are returned but with an unknown number that are missing
 
 
 def page_request(page):
+    # a function that will return one page of listings using GraphQL query
     graphql_endpoint = "https://api.ogjre.com/graphql"
 
-    # Define the GraphQL query and variables
+    # GraphQL query and variables
     graphql_query = """
       query VideosWithTranscripts($limit: Int, $offsetMultiplier: Int) {
         videosWithTranscripts(limit: $limit, offsetMultiplier: $offsetMultiplier) {
@@ -51,55 +52,59 @@ def page_request(page):
         'query': graphql_query,
         'variables': variables
     }
-    # episodes_listing_url = "https://ogjre.com/transcripts"
-
-    # make html request
-    # response = requests.get(episodes_listing_url)
 
     # Send the GraphQL request
     response = requests.post(graphql_endpoint, headers=headers, json=payload)
 
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
+
         # Parse and process the response (response.json())
         file_data = response.json()['data']['videosWithTranscripts']['videos']
-        # print(response.json())
-
-        # print(transcript)
 
         for file in file_data:
-            transcript = file['transcript']
-            transcript_real = transcript[len("null"):].strip()
+            # loop through each episode's data to extract id, title, type, and transcript
+            transcript_raw = file['transcript']
+
+            transcript = transcript_raw[len("null"):].strip()
             title = file["title"]
             id = file['id']
             type = file['type']
 
+            # add data into an episode specific dictionary
             episode_data = {"Episode ID": id,
                             "Video Type": type,
                             "Video Title": title,
-                            "Transcript": transcript_real}
+                            "Transcript": transcript}
 
+            # add episode dictionary to the larger dictionary
             full_dataset[episode_data["Episode ID"]] = episode_data
     else:
-        print(f"Failed to retrieve data. Status code: {response.status_code}")
+        print(f"Request failed. Status code: {response.status_code}")
 
+# create an empty dictionary to store episode information
 full_dataset = {}
 
-counter = 0
-for i in range(0,130+1):
+# create a for loop to request as many pages as needed to gather information for each video
+# around 135 is enough to cover all of the episodes.
+for i in range(0,150+1):
     try:
+        # use function page_request with the i variable dictating the page number
         page_request(i)
-        counter += 50
-        # print(counter)
-        print(len(full_dataset))
+        # print the length of the dictionary to display progress
+        dictlen = len(full_dataset)
+        print("")
+        print(f"Page: {i}")
+        print(f"Number of files: {dictlen}")
     except Exception as e:
-        print("No more episodes?")
+        print("Error encountered.")
 
-# write to csv
+# write dictionary to csv file in this directory
 
 csv_file_path = "JRE Transcripts.csv"
 
-with open(csv_file_path, 'w', newline='') as csvfile:
+with open(csv_file_path, 'w', newline='', encoding='utf-8') as csvfile:
+    # set column headings
     fieldnames = ["Episode ID", "Video Type", "Video Title", "Transcript"]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
